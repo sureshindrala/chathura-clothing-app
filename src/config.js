38cb4da20ae20487d57b8ab6353e0ce111db6d89
siva <siva@gmail.com>
2025-02-22 18:48:21 +0530

    added user product endpoints

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://34.132.242.203:5132/api'
-const usersServiceDevHost = 'http://34.132.242.203:5232/api'
+const produtsServiceDevHost = 'http://34.57.49.44:30263/api' 
+const usersServiceDevHost = 'http://34.57.49.44:31904/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
