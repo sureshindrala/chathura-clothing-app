d4ab557f6bc3d415f9fac013dda026779bf4d052
Suresh Indrala <sureshindrala99@gmail.com>
2025-10-04 18:22:09 +0530

    updated clothing app

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,7 +1,2 @@
 // create a jenkinsfile with all stages
 // Calling the shared library
-
-@Library("com.i27academy.slb@main") _
-nodePipeline(
-    appName: 'clothing'
-)
--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://104.198.52.21:30263/api' 
-const usersServiceDevHost = 'http://104.198.52.21:31904/api'
+const produtsServiceDevHost = 'http://136.114.27.219:5132/api' 
+const usersServiceDevHost = 'http://136.114.27.219:5232/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
